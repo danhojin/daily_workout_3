d89c3ca14793a2742e2d04f6cd13db8ef5c67c6b
danhojin <danhojin@gmail.com>
2021-04-06 11:58:08 +0900

    challenges base

--- lib/challenges.dart
+++ lib/challenges.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
 
-import 'package:daily_workout_3/main.dart';
 import 'package:daily_workout_3/models.dart';
 
 class ChallengesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<Challenge> items = context.watch<ItemsBoxState<Challenge>>().items;
+    items.sort((b, a) => a.created.compareTo(b.created));
     return Scaffold(
       appBar: AppBar(
         title: Text('Challenges'),
@@ -15,22 +16,34 @@ class ChallengesPage extends StatelessWidget {
       body: ListView.builder(
         itemCount: items.length,
         itemBuilder: (context, index) {
-          return _ExerciseDescription(title: items[index].exercise.toString());
+          return _ExerciseDescription(item: items[index]);
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
       ),
     );
   }
 }
 
 class _ExerciseDescription extends StatelessWidget {
-  final String title;
+  final Challenge item;
 
-  const _ExerciseDescription({Key? key, required this.title}) : super(key: key);
+  const _ExerciseDescription({Key? key, required this.item}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    String title = item.repetitions.map((el) => el.toString()).join('/');
+    String subtitle = DateFormat.yMMMMEEEEd().format(item.created);
+    subtitle = subtitle + ' (12 min 56 sec)';
     return ListTile(
+      leading: Icon(
+        Icons.explore,
+        size: 40.0,
+      ),
       title: Text(title),
+      subtitle: Text(subtitle),
     );
   }
 }
